09ff41511b466f400e9b94ec9909f6c0f6cbb8df
mdebayu <mdebayu@gmail.com>
2019-04-28 14:18:26 +1000

    Displaying all products to home page in a table

--- dazzlegift/frontend/src/components/products/Products.js
+++ dazzlegift/frontend/src/components/products/Products.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getProducts } from '../../actions/products';
@@ -15,12 +15,35 @@ export class Products extends Component {
 
 	render() {
 		return (
-			<div>
-				<h1> Products List </h1> 
-			</div>
+			<Fragment>
+				<h2> Products </h2>
+				<table className = "table tabled-striped"> 
+					<thead>
+						<tr>
+							<th> ID </th>
+							<th> Name </th>
+							<th> Email </th>
+							<th> Message </th>
+							<th /> 
+						</tr>
+					</thead>
+					<tbody>
+						{this.props.products.map(product => (
+							<tr key={product.id}>
+								<td>{product.id}</td>
+								<td>{product.name}</td>
+								<td>{product.email}</td>
+								<td>{product.message}</td>
+								<td><button className = "btn btn-danger btn-sm">Delete</button></td>
+							</tr>
+						))
+					}
+					</tbody>
+					
+				</table> 
+			</Fragment>
 			);
 	}
-
 }
 
 const mapStateToProps = state => ({
